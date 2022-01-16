c3d7eb7a4400265d6824975ad3be02a11de7f5c9
matavt <55859839+matavt@users.noreply.github.com>
2022-01-16 17:35:01 +0930

    Finished

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+//called when compute button in clicked
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    //validate the input for principal amount
+    if(principal<=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return; //return here to stop last line running if there is an error
+    }
+
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    return;
 }
-        
+
+//called when the interest rate slider is moved and released
+function updaterate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}        
